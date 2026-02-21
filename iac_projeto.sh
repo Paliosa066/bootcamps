42250bf96375e6ad3e5844fc0962168931950bea
Paliosa066 <132509107+Paliosa066@users.noreply.github.com>
2026-02-21 02:48:15 -0300

    iac_projeto.sh

--- iac_projeto.sh
+++ iac_projeto.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-echo "Iniciando a criação da infraestrutura..."
 
-echo "------------------------------------------"
-echo "Criando diretórios..."
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-echo "------------------------------------------"
-echo "Criando grupos de usuários..."
+
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "------------------------------------------"
-echo "Criando usuários e adicionando aos grupos..."
+
 # Usuários do setor ADM
 useradd carlos -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_ADM
@@ -32,8 +27,7 @@ useradd josefina -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_SEC
 
-echo "------------------------------------------"
-echo "Especificando permissões dos diretórios..."
+
 
 # Define o dono como root e os grupos correspondentes
 chown root:GRP_ADM /adm
@@ -48,5 +42,4 @@ chmod 770 /sec
 # Público: Todos podem ler, escrever e executar
 chmod 777 /publico
 
-echo "------------------------------------------"
-echo "Infraestrutura criada com sucesso!"
+
